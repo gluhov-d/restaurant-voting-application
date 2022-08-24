0e4bce49f2dbc266022e3b649f9aac08eddd92cf
Dmitrii Glukhov <glukhov.d@gmail.com>
2022-08-24 20:14:10 +0300

    init

---
+++ src/main/java/restaurantvoting/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package restaurantvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication {
+
+    public static void main(String[] args) {
+        System.out.format("Welcome to restaurant voting application!");
+    }
+
+}
